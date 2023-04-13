e5d1dd0b4a71da4c5e54ff5048e93e53f4304f0b
Branimir Križanec <brnasw@gmail.com>
2023-04-13 19:44:00 +0200

    Initial commit

---
+++ Unit testing/aspnet_core_unit_1.Tests/HomeControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using aspnet_core_unit_1.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+
+namespace aspnet_core_unit_1.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void CheckCountValueWithNumber500_ThrowsException ()
+        {          
+            // Arrange
+            var mockRepository = new Mock<ILogger<HomeController>>();
+            var mock = new HomeController(mockRepository.Object);
+            int id = 500;
+
+            // Act
+            Action actionCheck = () => mock.CheckCountValue(id);
+
+            // Assert
+            Exception exception = Assert.Throws<Exception>(actionCheck);
+            Assert.Equal("Broj je izvan raspona", exception.Message);
+        }
+
+        [Fact]
+        public void CheckCountValueWithNumber2 ()
+        {
+            // Arrange
+            var mockRepository = new Mock<ILogger<HomeController>>();
+            var mock = new HomeController(mockRepository.Object);
+            int id = 2;
+
+            // Act
+            var result = mock.CheckCountValue(id) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ Unit testing/aspnet_core_unit_1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using aspnet_core_unit_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace aspnet_core_unit_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult CheckCountValue(int i)
+        {
+            if (i < 20)
+            {
+                // Ovdje ide neka random logika
+            }
+            else
+            {
+                throw (new Exception("Broj je izvan raspona"));
+            }
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
