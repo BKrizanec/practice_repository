65912cae6b2af8a6371d1e70dd406863498b3cce
Branimir Križanec <brnasw@gmail.com>
2023-04-13 17:50:32 +0200

    Initial commit

---
+++ Simple Input Console App/Ispit.Konzola/Program.cs
@@ -0,0 +1,73 @@
+﻿using Ispit.Model;
+
+namespace Ispit.Konzola;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        bool appRunning = true;
+        List<Ucenik> ucenici = new List<Ucenik>();
+        Console.WriteLine("Ovo je aplikacija za unos osnovnih podataka učenika.");
+        
+        while (appRunning)
+        {            
+            Ucenik ucenik = new Ucenik();
+
+            Console.WriteLine();
+            Console.WriteLine("================================================================");
+            Console.WriteLine();            
+
+            Console.Write("Molimo unesite ime učenika: ");
+            ucenik.Ime = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Molimo unesite prezime učenika: ");
+            ucenik.Prezime = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Molimo unesite datum rođenja učenika u formatu mm/dd/yyyy: ");
+            if (DateTime.TryParse (Console.ReadLine(), out DateTime datum))
+            {
+                ucenik.DatumRodjenja = datum;
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Unos godišta nije uspješan. Provjerite jeste li unijeli ispravan format (mm/dd/yyyy).");
+                continue;
+            }
+
+            Console.Write("Molimo unesite prosjek učenika: ");
+            if (double.TryParse(Console.ReadLine(), out double prosjek))
+            {
+                ucenik.Prosjek = prosjek;
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Prosjek nije pravilno izračunat. Molimo da koristite ispravan format. Primjer uspješnog unosa: 4.3.");                
+                continue;
+            }
+
+            ucenici.Add(ucenik);
+
+            if(ucenici.Count == 3)
+            {
+                appRunning = false;
+            }
+        }
+
+        foreach (Ucenik ucenik in ucenici)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ime i prezime učenika: " + ucenik.Ime + " " + ucenik.Prezime);           
+            ucenik.Starost();
+            ucenik.IspisProsjeka();
+            Console.WriteLine("================================================================");
+            Console.WriteLine();
+        }
+        
+        Console.ReadKey();
+    }
+}
---
+++ Simple Input Console App/Ispit.Model/Ucenik.cs
@@ -0,0 +1,54 @@
+﻿namespace Ispit.Model;
+
+public class Ucenik
+{
+    public string Ime { get; set; }
+    public string Prezime { get; set;}
+    public DateTime DatumRodjenja { get; set; }
+    public double Prosjek { get; set; }
+
+    
+    public void Starost()
+    {
+        DateTime danas = DateTime.Today;            
+        int starost = danas.Year - DatumRodjenja.Year;
+
+        if (danas.Year > DatumRodjenja.Year)
+        {
+            Console.WriteLine("Godište učenika: " + DatumRodjenja.Year + ".");
+            Console.WriteLine("Starost učenika u godinama: " + starost + "."); 
+        }
+        else
+        {
+            Console.WriteLine("Unos godišta nije bio uspješan. Provjerite jeste li unijeli ispravan format (mm/dd/yyyy).");
+        }
+    }
+
+    public void IspisProsjeka()
+    {
+        if (Prosjek >= 1 && Prosjek <= 1.49)
+        {
+            Console.WriteLine("Prosjek učenika je: " + Prosjek + ". Uspjeh učenika je nedovoljan.");
+        }
+        else if (Prosjek >= 1.50 && Prosjek <= 2.49)
+        {
+            Console.WriteLine("Prosjek učenika je: " + Prosjek + ". Uspjeh učenika je dovoljan.");
+        }
+        else if (Prosjek >= 2.50 && Prosjek <= 3.49)
+        {
+            Console.WriteLine("Prosjek učenika je: " + Prosjek + ". Uspjeh učenika je dobar.");
+        }
+        else if (Prosjek >= 3.50 && Prosjek <= 4.49)
+        {
+            Console.WriteLine("Prosjek učenika je: " + Prosjek + ". Uspjeh učenika je vrlo dobar.");
+        }
+        else if (Prosjek >= 4.50 && Prosjek <= 5.00)
+        {
+            Console.WriteLine("Prosjek učenika je: " + Prosjek + ". Uspjeh učenika je odličan.");
+        }
+        else
+        {
+            Console.WriteLine("Prosjek nije pravilno izračunat. Molimo da koristite ispravan format. Primjer uspješnog unosa: 4.3.");                
+        }
+    }
+}
